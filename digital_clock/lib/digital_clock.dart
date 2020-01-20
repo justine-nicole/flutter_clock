1bd08d3022747cb477fa0a9ebf8ab63fc1dd5269
justine-nicole <justine.n.nugent@gmail.com>
2020-01-19 23:13:57 -0600

    Sunday Commit

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -3,32 +3,33 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+// Replace shadow with margin color
 enum _Element {
   background,
   text,
+  margin,
   shadow,
 }
 
-final _lightTheme = {
-  _Element.background: Color(0xFF81B3FE),
-  _Element.text: Colors.white,
-  _Element.shadow: Colors.black,
+// Define constant theme - mid century modern x himalayan salt lamp
+final _calmTheme = {
+  _Element.background: Color(0xDDEA9A71),
+  _Element.text: Color(0xFF6B300D),
+  _Element.margin: Color(0xDDFFE3B2),
+  _Element.shadow: Color(0xDDEA9A71),
 };
 
-final _darkTheme = {
-  _Element.background: Colors.black,
-  _Element.text: Colors.white,
-  _Element.shadow: Color(0xFF174EA6),
-};
+//final _darkTheme = {
+//  _Element.background: Colors.black,
+//  _Element.text: Colors.white,
+//  _Element.shadow: Color(0xFF174EA6),
+//};
 
 /// A basic digital clock.
-///
-/// You can do better than this!
 class DigitalClock extends StatefulWidget {
   const DigitalClock(this.model);
 
@@ -42,6 +43,7 @@ class _DigitalClockState extends State<DigitalClock> {
   DateTime _dateTime = DateTime.now();
   Timer _timer;
 
+  // Initialize everything and get the functions started
   @override
   void initState() {
     super.initState();
@@ -50,6 +52,8 @@ class _DigitalClockState extends State<DigitalClock> {
     _updateModel();
   }
 
+  // The widget updated, compare the current version to the old version
+  // and take the appropriate actions depending if they are still the same.
   @override
   void didUpdateWidget(DigitalClock oldWidget) {
     super.didUpdateWidget(oldWidget);
@@ -59,6 +63,7 @@ class _DigitalClockState extends State<DigitalClock> {
     }
   }
 
+  // Close up shop and release the resources
   @override
   void dispose() {
     _timer?.cancel();
@@ -67,15 +72,20 @@ class _DigitalClockState extends State<DigitalClock> {
     super.dispose();
   }
 
+  // Called so that setState will destroy the display and rebuild it
+  // for the next frame to be displayed
   void _updateModel() {
     setState(() {
       // Cause the clock to rebuild when the model changes.
     });
   }
 
+  // Use the milliseconds timer to see if we are toggling a new
+  // time increment. You can use a minute, or a second. Your choice.
   void _updateTime() {
     setState(() {
       _dateTime = DateTime.now();
+      //
       // Update once per minute. If you want to update every second, use the
       // following code.
       _timer = Timer(
@@ -93,39 +103,104 @@ class _DigitalClockState extends State<DigitalClock> {
     });
   }
 
+  // Ye old build method
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).brightness == Brightness.light
-        ? _lightTheme
-        : _darkTheme;
+    // Set the visual theme using if/else construct
+//    final colors = Theme.of(context).brightness == Brightness.light
+//        ? _lightTheme
+//        : _darkTheme;
+
+    // Set the visual theme to be constant
+    final colors = _calmTheme;
+    // Format the hour and minute according to user preferences
     final hour =
         DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
     final minute = DateFormat('mm').format(_dateTime);
-    final fontSize = MediaQuery.of(context).size.width / 3.5;
-    final offset = -fontSize / 7;
+    // set clock container height and font size
+    final containerHeight = MediaQuery.of(context).size.shortestSide / 2;
+    final containerWidth = MediaQuery.of(context).size.shortestSide / 10;
+    final fontSize = MediaQuery.of(context).size.shortestSide /5;
+    // set weather container height
+    final weatherHeight = MediaQuery.of(context).size.shortestSide;
+    final weatherWidth = MediaQuery.of(context).size.shortestSide / 1.6;
+    // align time display
+    final timePadding = widget.model.is24HourFormat
+        ? const EdgeInsets.fromLTRB(15, 0, 0, 0)
+        : const EdgeInsets.fromLTRB(10, 0, 0, 0);
+    // weather icon logic
+    // temperature bar logic
+
+
+    // Font Style Definition
     final defaultStyle = TextStyle(
       color: colors[_Element.text],
-      fontFamily: 'PressStart2P',
+      fontFamily: 'Lato',
       fontSize: fontSize,
       shadows: [
         Shadow(
-          blurRadius: 0,
+          blurRadius: 1,
           color: colors[_Element.shadow],
-          offset: Offset(10, 0),
+          offset: Offset(1, 0),
         ),
       ],
     );
 
-    return Container(
-      color: colors[_Element.background],
-      child: Center(
-        child: DefaultTextStyle(
-          style: defaultStyle,
-          child: Stack(
-            children: <Widget>[
-              Positioned(left: offset, top: 0, child: Text(hour)),
-              Positioned(right: offset, bottom: offset, child: Text(minute)),
-            ],
+    // Creating the UI
+    return SafeArea(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: colors[_Element.margin],
+        ),
+        child: Container(
+          color: colors[_Element.background],
+          height: containerHeight,
+          width: containerWidth,
+          margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Center(
+            child: Padding(
+              //add conditional stmt, time format
+              padding: timePadding,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    '$hour:$minute',
+                    style: defaultStyle,
+                  )),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  color: colors[_Element.background],
+                  height: weatherHeight,
+                  width: weatherWidth,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        color: colors[_Element.margin],
+                        height: weatherHeight,
+                        width: 12.0,
+                      ),
+                      // placeholder for weather condition icon
+                      Container(
+                        alignment: Alignment.centerRight,
+                        color: colors[_Element.background],
+                        height: 100.0,
+                        width: 100.0,
+                        child: Icon(Icons.ac_unit,
+                          color: Colors.white60,
+                          size: 80.0,
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+
+                ],
+              ),
+            ),
           ),
         ),
       ),
